0a011f97658e4f22dccb2009170964d4e4b86deb
Nina Sabado <nina.c.sabado@gmail.com>
2019-03-12 19:54:00 -0400

    Prints title, location, money raised

--- main.py
+++ main.py
@@ -9,5 +9,19 @@ url = 'https://www.gofundme.com/discover/medical-fundraiser'
 page = requests.get(url)
 
 soup = BeautifulSoup(page.text, 'html.parser')
-print(soup.prettify())
+tiles = soup.select(".js-fund-tile")
 
+for tile in tiles:
+  location = str.strip(tile.select_one(".fund-location").get_text())
+  title = str.strip(tile.select_one(".fund-title").get_text())
+  tile_items = tile.select(".fund-item .show-for-medium")
+
+  # get width for this
+  progress_bar = str.strip(tile.select_one(".progress-bar.progress-bar-fill")["style"])
+
+  # hacky way to get raised for now, there's no specific class for the amt
+  for tile_item in tile_items:
+    if "raised" in tile_item.get_text():
+       raised = str.strip(tile_item.get_text())
+
+  print(title,location,raised,progress_bar,"\n\n")
